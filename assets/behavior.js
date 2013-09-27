8192b74391079ce1c90ed2c51d143c839a79d80c
Lars Kappert <lars@webpro.nl>
2013-09-27 15:47:40 +0200

    Generated documentation for a4aca60b7c846ed2693fc57ce243b16855dd590f

--- assets/behavior.js
+++ assets/behavior.js
@@ -69,6 +69,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "slug": "events"
           },
           "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Polyfills",
+            "slug": "polyfills"
+          },
+          "depth": 2
         }
       ]
     }
