b94a09c01afc05b79dd168f5ecc9d22d99bd907f
Lars Kappert <lars@webpro.nl>
2013-11-24 18:23:16 +0100

    Add createFragment() to test helpers

--- test/lib/helpers.js
+++ test/lib/helpers.js
@@ -4,6 +4,20 @@ var getElement = function(element) {
         typeof element !== 'string' ?
             element :
             /^\s*<(\w+|!)[^>]*>/.test(element) ?
-                $(element) :
+                _createFragment(element) :
                 document.querySelectorAll(element);
+};
+
+var _createFragment = function(html) {
+
+    var fragment = document.createDocumentFragment(),
+        container = document.createElement('div');
+
+    container.innerHTML = html.trim();
+
+    while(container.firstChild) {
+        fragment.appendChild(container.firstChild);
+    }
+
+    return fragment.childNodes;
 };
