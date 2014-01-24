5e2525983a3f9b8eaad8e6c2142c894253fb4e9b
travis-ci <travis@travis-ci.org>
2014-01-24 15:21:34 +0000

    Update all the things

--- test/spec/html.js
+++ test/spec/html.js
@@ -17,6 +17,73 @@ describe('html', function() {
         expect(emptyContainer.html()).to.equal(html);
     });
 
+    it('should set the innerHTML of a <table> (<tr>)', function() {
+        var $table = $('<table/>'),
+            html = '<tr><td>1</td></tr>',
+            expected = '<tbody><tr><td>1</td></tr></tbody>';
+        $table.html(html);
+        expect($table[0].innerHTML).to.equal(expected);
+    });
+
+    it('should set the innerHTML of a <table> (<tr><tr>)', function() {
+        var $table = $('<table/>'),
+            html = '<tr><td>1</td></tr><tr><td>2</td></tr>',
+            expected = '<tbody>' + html + '</tbody>';
+        $table.html(html);
+        expect($table[0].innerHTML).to.equal(expected);
+    });
+
+    it('should set the innerHTML of a <table> (<td>)', function() {
+        var $table = $('<table/>'),
+            html = '<td>1</td>',
+            expected = '<tbody><tr><td>1</td></tr></tbody>';
+        $table.html(html);
+        expect($table[0].innerHTML).to.equal(expected);
+    });
+
+    it('should set the innerHTML of a <table> (<div>)', function() {
+        var $table = $('<table/>'),
+            html = '<div>1</div>';
+        $table.html(html);
+        expect($table[0].innerHTML).to.equal(html);
+    });
+
+    it('should set the innerHTML of a <select> (<option>)', function() {
+        var $select = $('<select/>'),
+            html = '<option>1</option>';
+        $select.html(html);
+        expect($select[0].innerHTML).to.equal(html);
+    });
+
+    it('should set the innerHTML of a <select> (<option><option>)', function() {
+        var $select = $('<select/>'),
+            html = '<option>1</option><option>2</option>';
+        $select.html(html);
+        expect($select[0].innerHTML).to.equal(html);
+    });
+
+    it('should set the innerHTML of a <select> (<div>)', function() {
+        var $select = $('<select/>'),
+            html = '<div>1</div>',
+            expected = '1';
+        $select.html(html);
+        expect($select[0].innerHTML).to.equal(expected);
+    });
+
+    it('should set the innerHTML of a <fieldset> (<legend>)', function() {
+        var $select = $('<fieldset/>'),
+            html = '<legend>1</legend>';
+        $select.html(html);
+        expect($select[0].innerHTML).to.equal(html);
+    });
+
+    it('should set the innerHTML of a <fieldset> (<div>)', function() {
+        var $select = $('<fieldset/>'),
+            html = '<div>1</div>';
+        $select.html(html);
+        expect($select[0].innerHTML).to.equal(html);
+    });
+
     it('should provide a chainable API', function() {
         var expected = emptyContainer;
         var actual = expected.html('');
