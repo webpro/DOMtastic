4bd0c30ea5a61dc611d6ff33c8774120fcd4fd57
Lars Kappert <lars@webpro.nl>
2014-11-16 15:35:10 +0100

    Update Zepto to v1.1.4 in benchmark

--- benchmark/init.js
+++ benchmark/init.js
@@ -16,7 +16,7 @@
         },
         {
             name: 'Zepto',
-            version: '1.1.3',
+            version: '1.1.4',
             script: {
                 src: 'vendor/zepto.min.js'
             }
