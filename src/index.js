ecb586dd19fa1b501e9d6d83e4bfa2677a1e8e6c
Lars Kappert <lars@webpro.nl>
2016-03-20 00:09:14 +0100

    Categorize internal modules

--- src/index.js
+++ src/index.js
@@ -11,21 +11,21 @@ let api = {},
 
 import * as array from './array';
 import BaseClass from './baseClass';
-import * as attr from './dom/attr';
-import * as class_ from './dom/class';
-import * as contains from './dom/contains';
 import * as css from './css';
-import * as data from './dom/data';
 import * as dom from './dom/index';
+import * as dom_attr from './dom/attr';
+import * as dom_class from './dom/class';
+import * as dom_contains from './dom/contains';
+import * as dom_data from './dom/data';
 import * as dom_extra from './dom/extra';
+import * as dom_html from './dom/html';
 import * as event from './event/index';
-import * as html from './dom/html';
+import * as event_trigger from './event/trigger';
+import * as event_ready from './event/ready';
 import * as noconflict from './noconflict';
-import * as ready from './event/ready';
 import * as selector from './selector/index';
-import * as closest from './selector/closest';
+import * as selector_closest from './selector/closest';
 import * as selector_extra from './selector/extra';
-import * as trigger from './event/trigger';
 import * as type from './type';
 
 if (typeof selector !== 'undefined') {
@@ -34,8 +34,8 @@ if (typeof selector !== 'undefined') {
     api.find = selector.find;
 }
 
-extend($, contains, noconflict, type);
-extend(api, array, attr, class_, closest, css, data, dom, dom_extra, event, html, ready, selector_extra, trigger);
+extend($, dom_contains, noconflict, type);
+extend(api, array, css, dom_attr, dom, dom_class, dom_data, dom_extra, dom_html, event, event_trigger, event_ready, selector_closest, selector_extra);
 
 $.fn = api;
 
