c8998f5c1f6301da13e3dc4299f37a82ec084130
Lars Kappert <lars@webpro.nl>
2017-03-20 22:09:31 +0100

    Remove vendor dir

--- build/gh-pages.sh
+++ build/gh-pages.sh
@@ -6,7 +6,6 @@ npm run coverage
 npm run doc
 rsync --recursive --delete ../dist/ dist
 rsync --recursive --delete ../test/ test
-rsync --recursive --delete ../vendor/ vendor
 rsync --recursive --delete ../coverage/ coverage
 rsync --recursive --delete ../doc/ doc
 rsync --recursive --delete ../examples/ examples
